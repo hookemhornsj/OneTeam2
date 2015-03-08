3cdfdb66b790f474cd23b9004dcc447c09777aba
Jeff LaMarca <jeffrey.j.lamarca@gmail.com>
2015-03-08 21:54:55 +0000

    Corrected associations syntax

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
   has_many :projects
   has_many :requests
   has_many :current_skills
-  has_many :skill, through: :current_skills
+  has_many :skills, through: :current_skills
   has_many :desired_skills
   has_many :skills, through: :desired_skills
 end
